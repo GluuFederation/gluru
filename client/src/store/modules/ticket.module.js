c6ce844cabb4395e067b51be6312c6e7dfbad243
lifelonglearner127 <life.long.learner127@outlook.com>
2018-08-05 23:36:16 -0700

    vue; add answer store logic

--- client/src/store/modules/ticket.module.js
+++ client/src/store/modules/ticket.module.js
@@ -1,8 +1,21 @@
 import {
+  FETCH_TICKET,
+  FETCH_ANSWERS,
   TICKET_CREATE,
-  TICKET_UPDATE
+  TICKET_UPDATE,
+  TICKET_DELETE,
+  ANSWER_CREATE,
+  ANSWER_UPDATE,
+  ANSWER_DELETE
 } from '@/store/actions.type'
+
+import {
+  SET_TICKET,
+  SET_ANSWERS
+} from '@/store/mutations.type'
+
 import TicketService from '@/services/ticket'
+import AnswerService from '@/services/answer'
 
 const initialState = {
   ticket: {
@@ -20,7 +33,8 @@ const initialState = {
     colleagues: '',
     private: '',
     link: ''
-  }
+  },
+  answers: []
 }
 
 export const state = Object.assign({}, initialState)
@@ -32,6 +46,25 @@ export const getters = {
 }
 
 export const actions = {
+  [FETCH_TICKET] (context, ticketSlug, prevTicket) {
+    if (prevTicket !== undefined) {
+      return context.commit(SET_TICKET, prevTicket)
+    }
+
+    return TicketService.get(ticketSlug)
+      .then(({ data }) => {
+        context.commit(SET_TICKET, data.ticket)
+        return data
+      })
+  },
+
+  [FETCH_ANSWERS] (context, ticketSlug) {
+    return AnswerService.get(ticketSlug)
+      .then(({ data }) => {
+        context.commit(SET_ANSWERS, data.comments)
+      })
+  },
+
   [TICKET_CREATE] ({ state }) {
     console.log('create')
     return TicketService.create(state.ticket)
@@ -39,11 +72,39 @@ export const actions = {
 
   [TICKET_UPDATE] ({ state }) {
     return TicketService.update(state.ticket.slug, state.ticket)
+  },
+
+  [TICKET_DELETE] (context, slug) {
+    return TicketService.destroy(slug)
+  },
+
+  [ANSWER_CREATE] (context, payload) {
+    return AnswerService
+      .post(payload.slug, payload.comment)
+      .then(() => { context.dispatch(FETCH_ANSWERS, payload.slug) })
+  },
+
+  [ANSWER_UPDATE] (context, payload) {
+
+  },
+
+  [ANSWER_DELETE] (context, payload) {
+    return AnswerService
+      .destroy(payload.slug, payload.answerId)
+      .then(() => {
+        context.dispatch(FETCH_ANSWERS, payload.slug)
+      })
   }
 }
 
 export const mutations = {
+  [SET_TICKET] (state, ticket) {
+    state.ticket = ticket
+  },
 
+  [SET_ANSWERS] (state, answers) {
+    state.answers = answers
+  }
 }
 
 export default {
--- client/src/store/mutations.type.js
+++ client/src/store/mutations.type.js
@@ -1,1 +1,2 @@
 export const SET_TICKET = 'setTicket'
+export const SET_ANSWERS = 'setAnswers'
