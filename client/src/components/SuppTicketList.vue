c7d6b9f1dd82500ebaa64b0b6284dd1c03263ac9
lifelonglearner127 <life.long.learner127@outlook.com>
2018-08-08 00:52:29 -0700

    vue; update ticket list component

--- client/src/components/SuppTicketList.vue
+++ client/src/components/SuppTicketList.vue
@@ -11,15 +11,87 @@
 <script>
 import { mapGetters } from 'vuex'
 import SuppTicketPreview from '@/components/SuppTicketPreview'
+import { FETCH_TICKETS } from '@/store/actions.type'
+
 export default {
   name: 'SuppTicketList',
   components: {
     SuppTicketPreview
   },
+  props: {
+    status: {
+      type: String,
+      required: false
+    },
+    category: {
+      type: String,
+      required: false
+    },
+    server: {
+      type: String,
+      required: false
+    },
+    os: {
+      type: String,
+      required: false
+    },
+    q: {
+      type: String,
+      required: false
+    },
+    itemPerPage: {
+      type: Number,
+      required: false
+    }
+  },
+  data () {
+    return {
+      currentPage: 1
+    }
+  },
   computed: {
+    listConfig () {
+      const filters = {
+        offset: (this.currentPage - 1) * this.itemPerPage,
+        limit: this.itemPerPage
+      }
+
+      if (this.status) {
+        filters.status = this.status
+      }
+
+      if (this.category) {
+        filters.category = this.category
+      }
+
+      if (this.server) {
+        filters.server = this.server
+      }
+
+      if (this.os) {
+        filters.os = this.os
+      }
+
+      if (this.q) {
+        filters.q = this.q
+      }
+      return {
+        filters
+      }
+    },
+    pages () {
+
+    },
     ...mapGetters([
-      'tickets'
+      'tickets',
+      'ticketsCount',
+      'isLoading'
     ])
+  },
+  methods: {
+    fetchTickets () {
+      this.$store.dispatch(FETCH_TICKETS, this.listConfig())
+    }
   }
 }
 </script>
